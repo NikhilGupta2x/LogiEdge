7b5885aaa3564f2a39b82e2a17109452876bc810
NikhilGupta2x <nikhiln22gupta@gmail.com>
2026-04-06 11:20:37 +0530

    updated Url

--- Frontend App/src/services/api.js
+++ Frontend App/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = "https://logiedge.onrender.com/api";
 
 // ── customer api calls ───────────────
 export const getAllCustomers = () => {
